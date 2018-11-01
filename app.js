56c4504f4baf6bca9871e7e06f96e61eaf2a3d06
Ricardo Suarez <ricardo.suarez.atencio@gmail.com>
2018-11-01 11:07:56 -0400

    Código principal
    Contiene el archivo app.js donde se encuentra toda la lógica del Chatbot y el archivo package.json de dependencias.

---
+++ app.js
@@ -0,0 +1,25 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("Tú dijiste: %s", session.message.text);
+});
